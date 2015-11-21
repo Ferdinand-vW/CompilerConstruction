5dd79e35bf7ec8623a3ec346265db4201f86a4ec
Ferdinand <ferdinandvwalree@gmail.com>
2015-11-21 16:52:03 +0100

    combined commits

--- tdiagrams/src/CCO/Picture/Base.hs
+++ tdiagrams/src/CCO/Picture/Base.hs
@@ -6,7 +6,7 @@ import CCO.Printing
 
 eval :: Diag -> Feedback Picture
 eval t = do
-        return $ Picture (200,200) $ pic_Syn_Diag $ wrap_Diag (sem_Diag t) (Inh_Diag (0,0))
+        return $ Picture (200,200) $ pic_Syn_Diag $ wrap_Diag (sem_Diag t) (Inh_Diag (0,200))
 
 --inh_Diag :: Inh_Diag
 --inh_Diag = Inh_Diag { prec_Inh_Diag = 0 }
