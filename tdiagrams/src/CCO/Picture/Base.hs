ed9f8a3fd2852208679e0848897f1d9578ae539f
Ferdinand <ferdinandvwalree@gmail.com>
2015-11-21 16:27:06 +0100

    Can now execute tdiad2picture

---
+++ tdiagrams/src/CCO/Picture/Base.hs
@@ -0,0 +1,12 @@
+module CCO.Picture.Base where
+
+import CCO.Picture.AG
+import CCO.Feedback
+import CCO.Printing
+
+eval :: Diag -> Feedback Picture
+eval t = do
+        return $ Picture (200,200) $ pic_Syn_Diag $ wrap_Diag (sem_Diag t) (Inh_Diag (0,0))
+
+--inh_Diag :: Inh_Diag
+--inh_Diag = Inh_Diag { prec_Inh_Diag = 0 }
--- tdiagrams/src/TDiag2Picture.hs
+++ tdiagrams/src/TDiag2Picture.hs
@@ -1,7 +1,8 @@
 import CCO.Component  (Component, component, printer, ioWrap)
 import CCO.Picture.AG (Diag (Diag))
+import CCO.Picture.Base
 import CCO.Picture    (Picture)
 import CCO.Tree       (ATerm, Tree (toTree,fromTree), parser)
 import Control.Arrow  (Arrow (arr),(>>>))
 
-main = ioWrap (parser >>> (component toTree :: Component ATerm Diag) >>> arr fromTree >>> printer)
+main = ioWrap (parser >>> (component toTree :: Component ATerm Diag) >>> component eval >>> arr fromTree >>> printer)
